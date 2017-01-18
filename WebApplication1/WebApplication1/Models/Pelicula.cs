15b9987406a7f796edcae635a0da93402238c231
VelizCorp <velizcorp@hotmail.com>
2017-01-18 15:56:43 -0430

    Nuevas ediciones

--- WebApplication1/WebApplication1/Models/Pelicula.cs
+++ WebApplication1/WebApplication1/Models/Pelicula.cs
@@ -7,6 +7,9 @@ namespace WebApplication1.Models
 {
     public class Pelicula
     {
-
+        public string Titulo { get; set; }
+        public int Duracion { get; set; }
+        public DateTime Publicacion { get; set; }
+        public string Pais { get; set; }
     }
 }
---
+++ WebApplication1/WebApplication1/Services/PeliculasService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public class PeliculasService
+    {
+        public Pelicula ObtenerPelicula(){
+            return new Pelicula()
+            {
+                Titulo = "Interestelar",
+                Duracion = 115,
+                Pais = "USA",
+                Publicacion = new DateTime(2013,11,26)
+            };
+        }
+        public List<Pelicula> ObtenerPeliculas()
+        {
+            var pelicula1 = new Pelicula()
+            {
+                Titulo = "Interestelar",
+                Duracion = 115,
+                Pais = "USA",
+                Publicacion = new DateTime(2013, 11, 26)
+            };
+            var pelicula2 = new Pelicula()
+            {
+                Titulo = "The NoteBook",
+                Duracion = 117,
+                Pais = "USA",
+                Publicacion = new DateTime(2013, 11, 26)
+            };
+
+            return new List<Pelicula> { pelicula1, pelicula2 };
+        }
+    }
+}
